62aa265c3d368f0c2fed952eebfb5847c3fea441
Slava <plumsoftwareofficial@gmail.com>
2025-03-30 17:03:46 +0600

    Апнул версию РСЯ

--- app/src/main/java/ru/plumsoftware/squid/game/data/AdsIds.java
+++ app/src/main/java/ru/plumsoftware/squid/game/data/AdsIds.java
@@ -1,9 +1,9 @@
 package ru.plumsoftware.squid.game.data;
 
 public class AdsIds {
-    public static final String OPEN_ADS_ID = "R-M-14203885-3";
-    public static final String BACKGROUND_SHOP_ACTIVITY_ADS_ID = "R-M-14203885-5";
-    public static final String SHOP_ACTIVITY_ADS_ID = "R-M-14203885-1";
-    public static final String BANNER_ADS_ID = "R-M-14203885-4";
-    public static final String REWARDED_ADS_ID = "R-M-14203885-2";
+    public static final String OPEN_ADS_ID = "R-M-13696131-3";
+    public static final String BACKGROUND_SHOP_ACTIVITY_ADS_ID = "R-M-13696131-1";
+    public static final String SHOP_ACTIVITY_ADS_ID = "R-M-13696131-5";
+    public static final String BANNER_ADS_ID = "R-M-13696131-2";
+    public static final String REWARDED_ADS_ID = "R-M-13696131-4";
 }
